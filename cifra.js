c677fc4d7501bbd636e156c67a36483376954f3f
Lucas da Paz <xlucaspx@gmail.com>
2022-12-13 10:57:14 -0300

    Melhorando Cifra de César

--- cifra.js
+++ cifra.js
@@ -1,23 +1,51 @@
-// versão resumida da cifra de césar
-const mensagem = 'secret message for test purposes';
+const mensagem = 'the answer is forty two';
 
+// matemática: https://danieldonda.com/a-matematica-da-cifra-de-cesar/
 function cifraMensagem(mensagem, movimentos) {
-  const mensagemSecreta = mensagem.split('').map(caractere => {
-    const codigoCaractere = caractere.charCodeAt(0);
-    return String.fromCharCode(codigoCaractere + movimentos);
-  });
-  return mensagemSecreta.join('');
+  let resultado = '';
+
+  for (let i = 0; i < mensagem.length; i++) {
+    const codigoCaractere = mensagem.charCodeAt(i);
+
+    // A = 65; Z = 90
+    if (codigoCaractere >= 65 && codigoCaractere <= 90) {
+      let calculoMovimentos = ((codigoCaractere - 65 + movimentos) % 26) + 65; // 26 = total de letras
+      resultado += String.fromCharCode(calculoMovimentos);
+
+      // a = 97; z = 122
+    } else if (codigoCaractere >= 97 && codigoCaractere <= 122) {
+      let calculoMovimentos = ((codigoCaractere - 97 + movimentos) % 26) + 97;
+      resultado += String.fromCharCode(calculoMovimentos);
+
+    } else {
+      resultado += String.fromCharCode(codigoCaractere);
+    }
+  }
+  return resultado;
 }
 
 function decifraMensagem(mensagem, movimentos) {
-  const mensagemSecreta = mensagem.split('').map(caractere => {
-    const codigoCaractere = caractere.charCodeAt(0);
-    return String.fromCharCode(codigoCaractere - movimentos);
-  });
-  return mensagemSecreta.join('');
+  let resultado = '';
+
+  for (let i = 0; i < mensagem.length; i++) {
+    const codigoCaractere = mensagem.charCodeAt(i);
+
+    if (codigoCaractere >= 65 && codigoCaractere <= 90) {
+      let calculoMovimentos = ((codigoCaractere - 90 - movimentos) % 26) + 90;
+      resultado += String.fromCharCode(calculoMovimentos);
+
+    } else if (codigoCaractere >= 97 && codigoCaractere <= 122) {
+      let calculoMovimentos = ((codigoCaractere - 122 - movimentos) % 26) + 122;
+      resultado += String.fromCharCode(calculoMovimentos);
+
+    } else {
+      resultado += String.fromCharCode(codigoCaractere);
+    }
+  }
+  return resultado;
 }
 
-const mensagemCifrada = cifraMensagem(mensagem,5 );
+const mensagemCifrada = cifraMensagem(mensagem, 5);
 console.log(mensagemCifrada);
 
 const mensagemDecifrada = decifraMensagem(mensagemCifrada, 5);
